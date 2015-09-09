69d7e6b24ff5636b22618fecd53cc368a6e022b5
chliam <chliam04@sina.com>
2015-09-09 08:21:23 +0800

    上次提交缺失的文件

---
+++ com.tk.dam/Entity/Jcd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.tk.dam.Entity
+{
+    /// <summary>
+    /// 监测点形变记录
+    /// </summary>
+    public class Jcd
+    {
+        public string Yqbh { get; set; }
+        public string Xh { get; set; }
+        public float Jzdgc { get; set; }
+        public float Bjgc { get; set; }
+        public int Zdcj1 { get; set; }
+        public int Jccj1 { get; set; }
+        public int Zdcj2 { get; set; }
+        public int Jccj2 { get; set; }
+    }
+}
