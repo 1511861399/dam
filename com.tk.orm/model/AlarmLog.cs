9a95c12300e7f11726ad2af3e2f67edd13b76df3
RuiQingChen <crq12@163.com>
2015-10-08 14:58:42 +0800

    生成alarmlog

---
+++ com.tk.orm/model/AlarmLog.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+namespace com.tk.orm.model
+{
+    /**
+     * armlogModel表数据交互对象
+     * annotation=@MessagePackMessage 
+     * Without annotation You register your class before use.
+     * MessagePack.register(MyClass.class);
+     */ 
+    public class AlarmLog 
+    {
+
+        /// <summary>
+        /// </summary>
+        public int aId{ get; set; }
+
+        /// <summary>
+        /// </summary>
+        public int sId{ get; set; }
+
+        /// <summary>
+        /// </summary>
+        public DateTime aDatetime{ get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string Event{ get; set; }
+
+        /// <summary>
+        /// </summary>
+        public int aLevel{ get; set; }
+    }
+}
